f1e73de7b9730ba1f4207a566e82f207f2ce3277
boshra-irnd <boshra.irnd@gmail.com>
2022-05-11 16:04:15 -0400

    add score model and article model

--- article/models.py
+++ article/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['created']
+
+class Score(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='article_score')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_score')
+    SCORE_CHOICES = [(num,num) for num in range(1,5)]
+    score = models.IntegerField(choices=SCORE_CHOICES)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['created']
