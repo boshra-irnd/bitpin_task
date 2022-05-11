b8c20e5b1d52c3801a3d2b4d46e2141f94726da1
boshra-irnd <boshra.irnd@gmail.com>
2022-05-11 16:55:12 -0400

    add article model to admin panel

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article, Score
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'body', 'created', 'updated']
+
